d273053fd67e5ff3a7df2258d6b62de01f11c65a
Sam Schiedermayer <sam.schiedermayer@liveality.com>
2018-08-29 11:45:52 -0500

    Added BoofCV and made slight changes to Robot.java

---
+++ TeamCode/src/main/java/lib_6981/Robot.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+
+public class Robot {
+    private static Robot currInstance;
+
+    public static Robot getInstance() {
+        return currInstance == null ? currInstance = new Robot() : currInstance;
+    }
+
+    List<String> flags = new CopyOnWriteArrayList<>();
+
+    interface Task {
+        void executeTasks();
+    }
+
+    private Robot() {
+    }
+
+    public void parallelProcess(String endTag, Task... tasks) {
+            CountDownLatch l = new CountDownLatch(tasks.length);
+            for (Task t : tasks) {
+                new Thread(() -> {
+                    t.executeTasks();
+                    l.countDown();
+                }).start();
+            }
+            new Thread(() -> {
+                while (l.getCount() != 0);
+                flags.add(endTag);
+            }).start();
+    }
+
+    public void waitForFlag(String flag) {
+        boolean flagFound = false;
+        while (!flagFound) {
+            for (String s : flags)
+                flagFound |= s.equals(flag);
+        }
+        flags.remove(flag);
+        System.out.println("Flag \"" + flag + "\" hit.");
+    }
+
+    public static void main(String[] args) {
+        Robot r = Robot.getInstance();
+        for (int j = 0; j < 1000; j++) {
+            r.parallelProcess("Breads", (() -> {
+//            try{Thread.sleep(0);}catch(Exception e){}
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        Thread.sleep((int) (Math.random() * 100));
+                    } catch (Exception e) {
+                    }
+                    System.out.println(i % 2 == 0 ? "Garlic" : "Wheat");
+                }
+            }), (() -> {
+                for (int i = 1; i <= 10; i++) {
+                    try {
+                        Thread.sleep((int) (Math.random() * 100));
+                    } catch (Exception e) {
+                    }
+                    System.out.println(i);
+                }
+            }));
+
+            r.waitForFlag("Breads");
+
+            System.out.println("Iteration " + j + " complete.");
+            System.out.println();
+        }
+
+        System.out.println("Program Complete");
+        System.out.println(r.flags);
+    }
+}
