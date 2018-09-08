46b7a3b236f599f11d91654cd93ef811fcf2ff53
Ben Nowotny <ben@hortonvillerobotics.com>
2018-09-08 18:03:55 -0500

    FileUtils Finished

--- TeamCode/src/main/java/lib_6981/FileUtils.java
+++ TeamCode/src/main/java/lib_6981/FileUtils.java
@@ -1,6 +1,46 @@
 package lib_6981;
 
+import android.os.Environment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class FileUtils {
-    static void writeTo(){}
+    public static void writeToFile(String fileName, Object contents){
+        try {
+            File f = new File(Environment.getExternalStorageDirectory() + "/" + fileName);
+            OutputStream o = new FileOutputStream(f);
+            o.write(contents.toString().getBytes());
+            o.flush();
+            o.close();
+        }catch(Exception e){e.printStackTrace();}
+    }
+
+    public static void appendToFile(String fileName, Object contents){
+        try{
+            File f = new File(Environment.getExternalStorageDirectory() + "/" + fileName);
+            OutputStream o = new FileOutputStream(f);
+            byte[] b = contents.toString().getBytes();
+            o.write(b, (int)f.length(), b.length);
+            o.flush();
+            o.close();
+        }catch(Exception e){e.printStackTrace();}
+    }
+
+    public static String readFromFile(String fileName){
+        try{
+            File f = new File(Environment.getExternalStorageDirectory() + "/" + fileName);
+            InputStream i = new FileInputStream(f);
+            byte[] b = new byte[(int)f.length()];
+            i.read(b);
+            return new String(b);
+        }catch(Exception e){e.printStackTrace();}
+        return null;
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRecorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRecorder.java
@@ -1,10 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+        import lib_6981.FileUtils;
 
 public class DriveRecorder extends LinearOpMode {
+
+    public static final double WHEEL_DIAMETER = 3.75;
+    public static final double GEAR_RATIO = 1;
+    public static final double TURN_DIAMETER = 18;
+    public static final double ENC_COUNTS_PER_ROTATION = 1120;
+
+    public static final String FILENAME = "opMode_Drive.java";
+
+    public static double backDrive_Drive(double encCounts){
+        return encCounts/ENC_COUNTS_PER_ROTATION*WHEEL_DIAMETER*Math.PI;
+    }
+
+    public static double backDrive_Turn(double encCounts){
+        return encCounts/ENC_COUNTS_PER_ROTATION*WHEEL_DIAMETER*Math.PI/TURN_DIAMETER/Math.PI*360*Math.signum(getEncoderCounts(mtrLeftDrive));
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
+        FileUtils.writeToFile(FILENAME, "package org.firstinspires.ftc.teamcode;\n" +
+                "\n" +
+                "import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;\n" +
+                "\n" +
+                "import lib_6981.FileUtils;\n" +
+                "\n" +
+                "public class opMode_Drive extends LinearOpMode {\n" +
+                "public final Robot robot = Robot.getInstance(this);\n"+
+                "    \n" +
+                "    @Override\n" +
+                "    public void runOpMode() throws InterruptedException {\n" +
+                "       robot.initialize();\n"+
+                "       while(!opModeIsActive){}\n");
 
+        FileUtils.writeToFile(FILENAME, "    }\n" +
+                "}\n");
     }
 }
