d28ea1c37aa23c2989b3eab159fa13a2466fcb14
Ben Nowotny <ben@hortonvillerobotics.com>
2018-11-25 10:57:53 -0600

    Force push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRecorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveRecorder.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.hortonvillerobotics.FinalRobotConfiguration;
 import com.hortonvillerobotics.Robot;
-import com.hortonvillerobotics.RobotConfiguration;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -10,43 +10,48 @@ import com.hortonvillerobotics.FileUtils;
 @TeleOp(name = "Recorder")
 public class DriveRecorder extends LinearOpMode {
 
-    public static final double WHEEL_DIAMETER = 3.75;
+    static final FinalRobotConfiguration f = new FinalRobotConfiguration();
+    public static final double WHEEL_CIRCUMFERENCE = f.wheelCircumference;
     public static final double GEAR_RATIO = 1;
-    public static final double TURN_DIAMETER = 18;
-    public static final double ENC_COUNTS_PER_ROTATION = 1120;
+    public static final double TURN_DIAMETER = f.turnDiameter;
+    public static final double ENC_COUNTS_PER_ROTATION = f.countsPerRotation;
     public static final double STD_POWER = 0.2;
     public int lockA = -1;
     public int lockB = -1;
-    Robot r;
+    Robot r = Robot.getInstance(this, f);;
 
     public static final String FILENAME = "/opMode_Drive.java";
 
     public double backDrive_Drive(double encCounts){
-        return Math.round(encCounts/GEAR_RATIO/ENC_COUNTS_PER_ROTATION*(WHEEL_DIAMETER*Math.PI)*100)/100.;
+        return Math.round(encCounts/GEAR_RATIO/ENC_COUNTS_PER_ROTATION*(WHEEL_CIRCUMFERENCE)*100)/100.;
     }
 
     public double backDrive_Turn(double encCounts){
-        return Math.round(encCounts/GEAR_RATIO/ENC_COUNTS_PER_ROTATION*WHEEL_DIAMETER*Math.PI/TURN_DIAMETER/Math.PI*360*Math.signum(r.getEncoderCounts("mtrLeftDrive"))*100)/100;
+        return Math.round(encCounts/GEAR_RATIO/ENC_COUNTS_PER_ROTATION* WHEEL_CIRCUMFERENCE /TURN_DIAMETER/Math.PI*360*Math.signum(r.getEncoderCounts("mtrLeftDrive"))*100)/100;
+    }
+
+    public Double backDrive_OWTurn(double encCounts){
+        return Math.round((encCounts/GEAR_RATIO/ENC_COUNTS_PER_ROTATION* WHEEL_CIRCUMFERENCE /TURN_DIAMETER/Math.PI*360)*100)/100.;
     }
 
 
     public void runOpMode() throws InterruptedException {
 
-        r = Robot.getInstance(this,new RobotConfiguration());
-        r.initialize(this,new RobotConfiguration());
+        r.initialize(this, f);
 
         FileUtils.writeToFile(FILENAME, "package org.firstinspires.ftc.teamcode;\n" +
                 "\n" +
                 "import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;\n" +
                 "\n" +
                 "import com.hortonvillerobotics.Robot;\n" +
+                "import com.hortonvillerobotics.FinalRobotConfiguration;\n" +
                 "\n" +
                 "public class opMode_Drive extends LinearOpMode {\n" +
-                "public final Robot robot = Robot.getInstance(this);\n"+
+                "public final Robot robot = Robot.getInstance(this, new FinalRobotConfiguration());\n"+
                 "    \n" +
                 "    @Override\n" +
                 "    public void runOpMode() throws InterruptedException {\n" +
-                "       robot.initialize();\n"+
+                "       robot.initialize(this, new FinalRobotConfiguration());\n"+
                 "       while(!opModeIsActive()){}\n"+
                 "\n");
 
@@ -61,24 +66,24 @@ public class DriveRecorder extends LinearOpMode {
             if(lockA != -1)
                 switch(lockA) {
                     case 1:
-                        r.setDrivePower(gamepad1.left_stick_y, gamepad1.left_stick_y);
+                        r.setDrivePower(0.1 * gamepad1.left_stick_y, 0.1 * gamepad1.left_stick_y);
                         break;
                     case 0:
-                        r.setDrivePower(gamepad1.right_stick_x, -gamepad1.right_stick_x);
+                        r.setDrivePower(0.1 * -gamepad1.right_stick_x, 0.1 * gamepad1.right_stick_x);
                         break;
                 }
 
             if(lockB != -1)
                 switch (lockB){
                     case 1:
-                        r.setDrivePower(gamepad2.left_stick_y, 0);
+                        r.setDrivePower( 0.1 * gamepad2.left_stick_y, 0);
                         break;
                     case 0:
-                        r.setDrivePower(0, gamepad2.right_stick_y);
+                        r.setDrivePower(0, 0.1 * gamepad2.right_stick_y);
                         break;
                 }
 
-            if(lockA != -1 && gamepad1.a && !gamepad1.start){
+            if(lockA != -1 && gamepad1.right_bumper){
                 switch(lockA){
                     case 0:
                         FileUtils.appendToFile(FILENAME, "      robot.Turn("+backDrive_Turn(-(Math.abs(r.getEncoderCounts("mtrLeftDrive"))+Math.abs(r.getEncoderCounts("mtrRightDrive")))/2)+", "+STD_POWER+");\n");
@@ -91,13 +96,13 @@ public class DriveRecorder extends LinearOpMode {
                 lockA = -1;
             }
 
-            if(lockB != -1 && gamepad2.a && !gamepad2.start&& !gamepad1.start){
+            if(lockB != -1 && gamepad2.right_bumper){
                 switch(lockB){
-                    case 0:
-                        FileUtils.appendToFile(FILENAME, "      robot.owTurn("+backDrive_Turn(-(Math.abs(r.getEncoderCounts("mtrLeftDrive"))+Math.abs(r.getEncoderCounts("mtrRightDrive")))/2)+", "+STD_POWER+");\n");
-                        break;
                     case 1:
-                        FileUtils.appendToFile(FILENAME, "      robot.owTurn("+backDrive_Drive(Math.signum(r.getEncoderCounts("mtrLeftDrive"))*(Math.abs(r.getEncoderCounts("mtrLeftDrive"))+Math.abs(r.getEncoderCounts("mtrRightDrive")))/2)+", "+STD_POWER+");\n");
+                        FileUtils.appendToFile(FILENAME, "      robot.owTurn("+backDrive_OWTurn(Math.abs(r.getEncoderCounts("mtrLeftDrive")))+", "+STD_POWER*Math.signum(r.getEncoderCounts("mtrLeftDrive"))+");\n");
+                        break;
+                    case 0:
+                        FileUtils.appendToFile(FILENAME, "      robot.owTurn("+-backDrive_OWTurn(Math.abs(r.getEncoderCounts("mtrRightDrive")))+", "+STD_POWER*Math.signum(r.getEncoderCounts("mtrRightDrive"))+");\n");
                         break;
                 }
                 r.resetDriveEncoders();
@@ -111,7 +116,7 @@ public class DriveRecorder extends LinearOpMode {
 
         }
 
-        if(lockA != -1){
+        if(lockA != -1 && gamepad1.right_bumper){
             switch(lockA){
                 case 0:
                     FileUtils.appendToFile(FILENAME, "      robot.Turn("+backDrive_Turn(-(Math.abs(r.getEncoderCounts("mtrLeftDrive"))+Math.abs(r.getEncoderCounts("mtrRightDrive")))/2)+", "+STD_POWER+");\n");
@@ -122,6 +127,19 @@ public class DriveRecorder extends LinearOpMode {
             }
             r.resetDriveEncoders();
             lockA = -1;
+        }
+
+        if(lockB != -1 && gamepad2.right_bumper){
+            switch(lockB){
+                case 1:
+                    FileUtils.appendToFile(FILENAME, "      robot.owTurn("+backDrive_OWTurn(Math.abs(r.getEncoderCounts("mtrLeftDrive")))+", "+STD_POWER*Math.signum(r.getEncoderCounts("mtrLeftDrive"))+");\n");
+                    break;
+                case 0:
+                    FileUtils.appendToFile(FILENAME, "      robot.owTurn("+-backDrive_OWTurn(Math.abs(r.getEncoderCounts("mtrRightDrive")))+", "+STD_POWER*Math.signum(r.getEncoderCounts("mtrRightDrive"))+");\n");
+                    break;
+            }
+            r.resetDriveEncoders();
+            lockB = -1;
         }
 
         FileUtils.appendToFile(FILENAME, "    }\n" +
