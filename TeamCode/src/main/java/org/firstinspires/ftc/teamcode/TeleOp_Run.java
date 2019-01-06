5c865ac16388cc43a2c3a8849481a670332ab83f
Ben Nowotny <ben@hortonvillerobotics.com>
2019-01-06 15:48:26 -0600

    Added locking control for the lift and set up the motors to be in the proper run mode.  Additionally, motors that are not being used are disabled.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Run.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Run.java
@@ -19,8 +19,11 @@ public class TeleOp_Run extends LinearOpMode {
     Robot r = Robot.getInstance(this, new FinalRobotConfiguration());
 
     boolean isLocked = true;
+    public final double LOCKCLOSED = 0.112;
+    public final double LOCKOPEN = 0.536;
+    boolean lockOS = false;
     double srvPos = 0;
-    double posLocked = 0, posOpen = 1;
+//    double posLocked = 0, posOpen = 1;
     Telemetry.Item encVol;
 
     String srvColL = "srvColL", srvColR = "srvColR", srvLock = "srvLock";
@@ -30,8 +33,8 @@ public class TeleOp_Run extends LinearOpMode {
         r.initialize(this, new FinalRobotConfiguration());
         r.setDriveRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         r.setRunMode("mtrLift", DcMotor.RunMode.RUN_USING_ENCODER);
-        r.setRunMode("mtrCrane", DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        r.setRunMode("mtrLin", DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        r.setRunMode("mtrCrane", DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        r.setRunMode("mtrLin", DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         while(!opModeIsActive()){}
 
         encVol = telemetry.addData("LiftVelocity", r.calculateVelocity(() -> r.getEncoderCounts("mtrLift"), 50));
@@ -50,33 +53,33 @@ public class TeleOp_Run extends LinearOpMode {
 
             r.setDrivePower(Math.abs(gamepad1.left_stick_y) >= 0.05 ? (gamepad1.right_bumper ? 0.3 : 1) * gamepad1.left_stick_y : 0, Math.abs(gamepad1.right_stick_y) >= 0.05 ? (gamepad1.right_bumper ? 0.3 : 1) * gamepad1.right_stick_y : 0);
             r.setPower("mtrLift", gamepad1.dpad_up ? 1 : gamepad1.dpad_down ? -1 : 0);
-            if(gamepad1.x) {
-                isLocked = false;
-            } else if(gamepad1.y) {
-                isLocked = true;
-            }
+
+            if(gamepad1.x && !lockOS){
+                lockOS = true;
+                isLocked=!isLocked;
+            }else if(!gamepad1.x) lockOS = false;
 
             if(isLocked) {
-                r.setServoPosition(srvLock,posLocked);
+                r.setServoPosition("srvLock",LOCKCLOSED);
             } else {
-                r.setServoPosition(srvLock,posOpen);
+                r.setServoPosition("srvLock",LOCKOPEN);
             }
 
             //CONTROLLER 2
             //CONTROLLER 2
 
-            r.setPower("mtrCrane",gamepad2.left_stick_y);
-            r.setPower("mtrLin",gamepad2.right_stick_y);
-            if(gamepad2.right_trigger > .5) {
-                r.setServoPower(srvColL,1);
-                r.setServoPower(srvColR,-1);
-            } else if(gamepad2.right_bumper) {
-                r.setServoPower(srvColL,-1);
-                r.setServoPower(srvColR,1);
-            } else {
-                r.setServoPower(srvColL,0);
-                r.setServoPower(srvColR,0);
-            }
+//            r.setPower("mtrCrane",gamepad2.left_stick_y);
+//            r.setPower("mtrLin",gamepad2.right_stick_y);
+//            if(gamepad2.right_trigger > .5) {
+//                r.setServoPower(srvColL,1);
+//                r.setServoPower(srvColR,-1);
+//            } else if(gamepad2.right_bumper) {
+//                r.setServoPower(srvColL,-1);
+//                r.setServoPower(srvColR,1);
+//            } else {
+//                r.setServoPower(srvColL,0);
+//                r.setServoPower(srvColR,0);
+//            }
 
 //            if (gamepad1.left_trigger >= 0.5 && srvTimer.getTimeElapsed() >= 20) {
 //                srvPos = srvPos < 1 ? srvPos + 0.008 : 1;
@@ -91,6 +94,7 @@ public class TeleOp_Run extends LinearOpMode {
 
 
             //UPDATING TELEMETRY FOR THE USER
+            telemetry.addData("isLocked", isLocked);
             for(Object s : r.motors.keySet()){
                 telemetry.addData((String) s, r.getPower((String)s));
                 telemetry.addData((String) s + " enc", r.getEncoderCounts((String) s));
